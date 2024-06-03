0c7eeee89c8b79b243378090c94c10f905d83380
Kwame Mintah <kwame_mintah@hotmail.co.uk>
2024-06-03 19:58:01 +0100

    feat(provider): enable purge deleted keys on destory
    ensure key vaults are completely deleted on destorys

--- provider.tf
+++ provider.tf
@@ -2,7 +2,8 @@
 provider "azurerm" {
   features {
     key_vault {
-      purge_soft_delete_on_destroy = true
+      purge_soft_delete_on_destroy       = true
+      purge_soft_deleted_keys_on_destroy = true
     }
   }
   tenant_id       = var.arm_tenant_id
